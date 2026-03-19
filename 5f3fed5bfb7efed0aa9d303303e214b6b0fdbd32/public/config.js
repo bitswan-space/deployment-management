41a4aaa658b416372ca38ed40b0b01ecc5204d19
gitops <gitops@gitops.com>
2026-03-19 17:59:12 +0000

    Add asset 5f3fed5bfb7efed0aa9d303303e214b6b0fdbd32

---
+++ 5f3fed5bfb7efed0aa9d303303e214b6b0fdbd32/public/config.js
@@ -0,0 +1,7 @@
+window.__BITSWAN_CONFIG__ = {
+  workspaceName: "editor-sandbox",
+  deploymentId: "internal-frontend-live-dev",
+  stage: "live-dev",
+  domain: "sandbox.bitswan.ai",
+  urlTemplate: "https://editor-sandbox-{name}-live-dev.sandbox.bitswan.ai"
+};
