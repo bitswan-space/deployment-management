58814b4e477dbe0e07385763e0d5c62d2424681e
gitops <gitops@gitops.com>
2026-04-08 22:33:59 +0000

    Add asset c9455c96c559c022059d9fe08d3b1324ab5b2307

---
+++ c9455c96c559c022059d9fe08d3b1324ab5b2307/public/config.js
@@ -0,0 +1,14 @@
+window.__BITSWAN_CONFIG__ = {
+  workspaceName: "deployment-management",
+<<<<<<< Updated upstream
+  deploymentId: "external-frontend-wt-coding-agent-staging-deployment-management-live-dev",
+  stage: "live-dev",
+  domain: "bitswan-devops-1.bswn.io",
+  urlTemplate: "https://deployment-management-{name}-dca1-live-dev.bitswan-devops-1.bswn.io"
+=======
+  deploymentId: "external-frontend-deployment-management-live-dev",
+  stage: "live-dev",
+  domain: "bitswan-devops-1.bswn.io",
+  urlTemplate: "https://deployment-management-{name}-deployment-management-live-dev.bitswan-devops-1.bswn.io"
+>>>>>>> Stashed changes
+};
