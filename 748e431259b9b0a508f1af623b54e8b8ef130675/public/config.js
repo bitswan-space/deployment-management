742e10d62d0616f2002ec5ab425216555da6ea66
gitops <gitops@gitops.com>
2026-04-20 18:06:50 +0000

    Add asset 748e431259b9b0a508f1af623b54e8b8ef130675

---
+++ 748e431259b9b0a508f1af623b54e8b8ef130675/public/config.js
@@ -0,0 +1,11 @@
+window.__BITSWAN_CONFIG__ = {
+  workspaceName: "deployment-management",
+  deploymentId: "external-frontend-deployment-management-live-dev",
+  stage: "live-dev",
+  domain: "bitswan-devops-1.bswn.io",
+<<<<<<< Updated upstream
+  urlTemplate: "https://deployment-management-{name}-d86b-live-dev.bitswan-devops-1.bswn.io"
+=======
+  urlTemplate: "https://deployment-management-{name}-deployment-management-live-dev.bitswan-devops-1.bswn.io"
+>>>>>>> Stashed changes
+};
